7a7d39d0bb232fb0e40df8994afd9c12df41206c
sarahheaps <sarahheaps@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2014-09-19 16:16:11 +0000

    Added source.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/nseg4-cdt-mod@2 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ pkg/R/cdtRmodelling-package.R
@@ -0,0 +1,11 @@
+#' R courses for Newcastle University's CDT in Cloud 
+#' Computing for Big Data
+#' 
+#' Functions and datasets used for Newcastle University's 
+#' CDT in Cloud Computing for Big Data. This package is 
+#' used in the "Statistical Modelling" course.
+#' @name cdtRmodelling-package
+#' @aliases cdtRmodelling
+#' @docType package
+#' @keywords package
+NULL
